3b81fe647d0fc90fd6962bb6ae9c54815f7713aa
Radu Pogonariu <radu.pogonariu@gmail.com>
2022-10-05 11:52:29 +0200

    Add is_disabled field to Judge

--- judge/models/runtime.py
+++ judge/models/runtime.py
@@ -129,6 +129,8 @@ class Judge(models.Model):
     is_blocked = models.BooleanField(verbose_name=_('block judge'), default=False,
                                      help_text=_('Whether this judge should be blocked from connecting, '
                                                  'even if its key is correct.'))
+    is_disabled = models.BooleanField(verbose_name=_('disable judge'), default=False,
+                                      help_text=_('Whether this judge should be removed from judging queue.'))
     online = models.BooleanField(verbose_name=_('judge online status'), default=False)
     start_time = models.DateTimeField(verbose_name=_('judge start time'), null=True)
     ping = models.FloatField(verbose_name=_('response time'), null=True)
@@ -146,6 +148,13 @@ class Judge(models.Model):
         disconnect_judge(self, force=force)
 
     disconnect.alters_data = True
+
+    def toggle_disabled(self):
+        self.is_disabled = not self.is_disabled
+        self.save(update_fields=['is_disabled'])
+        disconnect_judge(self, force=True)
+
+    toggle_disabled.alters_data = True
 
     @classmethod
     def runtime_versions(cls):

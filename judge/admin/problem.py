de9f15adeddb9acbebf6ee52fb93c2e44402c023
FherStk <porrino.fernando@gmail.com>
2023-12-27 11:14:34 +0000

    Infering test cases from a loaded zip file or an existing yml file, also able to hide test-cases from the problem statement respecting the judge order (pretest firsts).

--- judge/admin/problem.py
+++ judge/admin/problem.py
@@ -125,7 +125,7 @@ class ProblemAdmin(NoBatchDeleteMixin, VersionAdmin):
             'fields': (
                 'code', 'name', 'is_public', 'is_manually_managed', 'date', 'authors', 'curators', 'testers',
                 'organizations', 'submission_source_visibility_mode', 'is_full_markup',
-                'description',
+                'description', 'include_test_cases',
             ),
         }),
         (_('Authorship'), {'fields': (('authorship_name', 'authorship_uri'), 'license')}),
